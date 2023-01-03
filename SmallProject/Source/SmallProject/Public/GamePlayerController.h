0ee581012e620126eb05ac7f318f2291e9305ca7
gregtom6 <gregtom6@gmail.com>
2023-01-03 17:39:11 +0100

    setting lock mode for cursor during normal gameplay

---
+++ SmallProject/Source/SmallProject/Public/GamePlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GamePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMALLPROJECT_API AGamePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AGamePlayerController();
+};
