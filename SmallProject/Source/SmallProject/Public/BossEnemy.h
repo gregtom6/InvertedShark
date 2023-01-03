4ecdc445462cd584c335facd548de43b92692dc5
gregtom6 <gregtom6@gmail.com>
2023-01-03 21:11:19 +0100

    making fly easier during boss battle

---
+++ SmallProject/Source/SmallProject/Public/BossEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "BossEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMALLPROJECT_API ABossEnemy : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
